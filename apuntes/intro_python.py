2439e79fc0bde7f3a6d30249f9f1acced095f284
facusteckler86 <facundomsteckler@gmail.com>
2026-04-17 23:13:49 -0300

    sigo cargando data en intro python

--- apuntes/intro_python.py
+++ apuntes/intro_python.py
@@ -2,17 +2,22 @@
 
 print("Hola Mundo")
 
-
-
-# Variables
+# Variables - uso del for
 variable = int
-
 
 if variable:
     variable = variable 
 
 
 # Tipos de variables
+
+nombre = "Facundo"
+apellido = "Steckler"
+print(nombre + " " + apellido)
+
+# Uso de type para poder saber el tipo de variable
+
+print=type(nombre)
 
 int = 5
 
@@ -33,6 +38,33 @@ print(3**2)
 print(15/2)
 print(15//2)
 print(7%5)
+
+# Orden de procedencia:
+
+# En matematica siempre se resuelve de izquierda a derecha, evaluando estas condiciones:
+
+# terminos entre parentesis
+# potencias y raices
+# multiplicacion y division
+#suma y resta
+
+# Cademas con el uso de print()
+
+print("Una cadena con \tcon tabulacion")
+print("Otra cadena\ncon salto de linea")
+
+# Forma de calcular un string
+
+# function len
+
+len("Facundo")
+
+# Slicing (recorte de strings)
+
+nombre_completo= "facundo steckler"
+nombre_completo[4:7] # se indica desde que caracter se corta
+
+
 
 
 
