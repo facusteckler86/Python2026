a85b5a4ad6717dd863db2b025eeb6cfa5eed7580
facusteckler86 <facundomsteckler@gmail.com>
2026-05-01 22:58:15 -0300

    agrego ejercicios de practica

--- ejercicios/ejercicios.py
+++ ejercicios/ejercicios.py
@@ -20,3 +20,22 @@ print(lista4)
 
 lista5 = lista2 + lista3
 print(lista5)
+
+# /* 
+#  * Escribe un programa que muestre por consola (con un print) los
+#  * números de 1 a 100 (ambos incluidos y con un salto de línea entre
+#  * cada impresión), sustituyendo los siguientes:
+#  * - Múltiplos de 3 por la palabra "fizz".
+#  * - Múltiplos de 5 por la palabra "buzz".
+#  * - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
+#  */
+
+for i in range(1, 101):
+    if i % 3 == 0 and i % 5 == 0:
+        print("fizzbuzz")
+    elif i % 3 == 0:
+        print("fizz")
+    elif i % 5 == 0:
+        print("buzz")
+    else:
+        print(i)
